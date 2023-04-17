46223e4f04b7ea0099b1c784580b258de19303ee
PavelSBakhtin <113133223+PavelSBakhtin@users.noreply.github.com>
2023-04-17 18:36:42 +0300

    home work 1 done

--- home_work_1.sql
+++ home_work_1.sql
@@ -7,7 +7,7 @@ CREATE TABLE home_work_1.nomenclature (
   PRIMARY KEY (id));
 INSERT INTO home_work_1.nomenclature (manufacturer, model, price) VALUES ('Apple', 'iPhone', 99.99);
 INSERT INTO home_work_1.nomenclature (manufacturer, model, price) VALUES ('Samsung', 'Galaxy S21 Plus', 59.99),
-	('Samsung', 'Galaxy A32', 49.99), ('Samsung', 'Galaxy M51', 39.99), ('Xiaomi', 'Mi 10T Pro', 79.99),
+    ('Samsung', 'Galaxy A32', 49.99), ('Samsung', 'Galaxy M51', 39.99), ('Xiaomi', 'Mi 10T Pro', 79.99),
     ('Xiaomi', 'Redmi Note 9 Pro', 69.99), ('HUAWEI', 'P40 Pro', 49.99), ('HUAWEI', 'P Smart', 39.99),
     ('Realme', '6 Pro', 69.99), ('Realme', 'C3', 59.99), ('Nokia', '8.3', 49.99), ('Nokia', '3.4', 39.99),
     ('ZTE', 'Blade A5', 29.99), ('Vivo', 'V20', 49.99), ('OPPO', 'Reno4', 39.99);
